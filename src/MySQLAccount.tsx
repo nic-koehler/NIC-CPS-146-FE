ac7a328767603f279045a5a40448783289e17583
Brian Koehler <brian@koehler.ca>
2021-08-10 20:55:37 -0700

    made UI a bit more intuitive

--- src/MySQLAccount.tsx
+++ src/MySQLAccount.tsx
@@ -4,32 +4,24 @@ import {
   useGoogleReCaptcha
 } from 'react-google-recaptcha-v3';
 
-interface Request {
-  email: string,
-};
+const FORM_STATE_DATA_ENTRY = 0;
+const FORM_STATE_WAITING = 1;
+const FORM_STATE_SUCCESS = 2;
+const FORM_STATE_ERROR = 3;
 
-function MySQLAccount() {
+function MySQLAccountForm(props: any) {
   const [email, setEmail] = useState("");
   const { executeRecaptcha } = useGoogleReCaptcha();
-  const [completed, setCompleted] = useState( false );
-  const [disableButton, setDisableButton] = useState( false );
-
-  let formDivClass = 'col-md-12 order-md-1';
-  let alertClass = 'd-none alert alert-success';
-  if ( completed ) {
-    formDivClass = 'd-none col-md-12 order-md-1';
-    alertClass = 'alert alert-success';
-  }
 
   const handleSubmit = async (e: FormEvent) => {
       e.preventDefault();
       const element = e.currentTarget as HTMLFormElement;
       if ( element.checkValidity() ) {
+        props.setFormState( FORM_STATE_WAITING );
         ReactGA.event({
           category: "Request",
           action: "Add",
         });
-        setDisableButton( true );
         let token = '';
         if ( executeRecaptcha ) {
           token = await executeRecaptcha!( 'request' );
@@ -50,16 +42,68 @@ function MySQLAccount() {
         .then( (response: any) => response.json() )
         .then( (data: any) => {
           console.log( data );
-          setEmail('');
-          setCompleted( true );
+          props.setFormState( FORM_STATE_SUCCESS );
         })
         .catch( (err: any) => {
           console.log( "There was a problem creating a new account request.");
+          props.setFormState( FORM_STATE_ERROR );
         });
       } else {
         element.classList.add('was-validated');
       }
   }
+
+  return(
+    <div className='col-md-12 order-md-1'>
+      <form onSubmit={handleSubmit} noValidate>
+        <div className="row">
+          <div className="col mb-3">
+            <label htmlFor="email">Email address</label>
+            <input type="email" className="form-control" id="email"
+              value={email} onChange={e => setEmail( e.target.value )}
+              required
+              pattern=".+@nic\.bc\.ca|.+@northislandcollege\.ca|.+@koehler.ca" />
+            <div className="invalid-feedback">
+              A valid email address is required.
+            </div>
+          </div>
+        </div>
+        <button className="btn btn-primary btn-lg btn-block"
+                type="submit">Request MySQL Account</button>
+      </form>
+    </div>
+  );
+}
+
+function MySQLAccountSuccess() {
+  return(
+    <div className='alert alert-success' role="alert">
+      Check your email for a link and further instructions.
+    </div>
+  );
+}
+
+function MySQLAccountError() {
+  return(
+    <div className='alert alert-danger' role="alert">
+      There was a problem with your request. Come back and try again later.
+    </div>
+  );
+}
+
+function MySQLAccountWaiting() {
+  return(
+    <div>
+      <div className="spinner-border m-3" role="status">
+        <span className="visually-hidden">processing...</span>
+      </div>
+      <span>processing...</span>
+    </div>
+  );
+}
+
+function MySQLAccount() {
+  const [formState, setFormState] = useState( FORM_STATE_DATA_ENTRY );
 
   return (
     <div>
@@ -73,28 +117,10 @@ function MySQLAccount() {
       </div>
 
       <div className="row">
-        <div className={formDivClass}>
-          <form onSubmit={handleSubmit} noValidate>
-            <div className="row">
-              <div className="col mb-3">
-                <label htmlFor="email">Email address</label>
-                <input type="email" className="form-control" id="email"
-                  value={email} onChange={e => setEmail( e.target.value )}
-                  required
-                  pattern=".+@nic\.bc\.ca|.+@northislandcollege\.ca|.+@koehler.ca" />
-                <div className="invalid-feedback">
-                  A valid email address is required.
-                </div>
-              </div>
-            </div>
-            <button className="btn btn-primary btn-lg btn-block"
-                    disabled={disableButton}
-                    type="submit">Request MySQL Account</button>
-          </form>
-        </div>
-        <div className={alertClass} role="alert">
-          Check your email for a link and further instructions.
-        </div>
+        { formState === FORM_STATE_DATA_ENTRY && <MySQLAccountForm setFormState={setFormState} />}
+        { formState === FORM_STATE_WAITING && <MySQLAccountWaiting />}
+        { formState === FORM_STATE_SUCCESS && <MySQLAccountSuccess />}
+        { formState === FORM_STATE_ERROR && <MySQLAccountError />}
       </div>
 
     </div>
--- src/MySQLAccountVerify.tsx
+++ src/MySQLAccountVerify.tsx
@@ -9,53 +9,52 @@ interface VerifyProps {
   }
 }
 
-function MySQLAccountVerify(props: VerifyProps) {
-  const [operation, setOperation] = useState( 'Create' );
-  const [email, setEmail] = useState( '' );
+const FORM_STATE_DATA_ENTRY = 0;
+const FORM_STATE_WAITING = 1;
+const FORM_STATE_SUCCESS = 2;
+const FORM_STATE_ERROR = 3;
+const FORM_STATE_LOADING = 4;
+
+function MySQLAccountVerifyWaiting(props: any) {
+  return(
+    <div>
+      <div className="spinner-border m-3" role="status">
+        <span className="visually-hidden">{props.message}</span>
+      </div>
+      <span>{props.message}</span>
+    </div>
+  );
+}
+
+function MySQLAccountVerifyError(props: any) {
+  return(
+    <div className='alert alert-danger' role="alert">
+      {props.message}
+    </div>
+  );
+}
+
+function MySQLAccountVerifySuccess(props: any) {
+  return(
+    <div className='alert alert-success' role="alert">
+      {props.message}
+    </div>
+  );
+}
+
+
+function MySQLAccountVerifyForm(props: any) {
   const [password, setPassword] = useState( '' );
-  const [disableButton, setDisableButton] = useState( false );
-  const [completed, setCompleted] = useState( false );
-  const [alertType, setAlertType] = useState( 'success' );
-  const [message, setMessage] = useState( 'OK' );
-
-  let formDivClass = 'col-md-12 order-md-1';
-  let alertClass = `d-none alert alert-${alertType}`;
-  if ( completed ) {
-    formDivClass = 'd-none col-md-12 order-md-1';
-    alertClass = `alert alert-${alertType}`;
-  }
-
-  useEffect(()=>{
-    fetch( process.env.REACT_APP_BACKEND_URL +
-      '/requests/' +
-      props.match.params.token )
-    .then( (response: any) => response.json() )
-    .then( (data: any) => {
-      console.log( data );
-      if ( data.message.startsWith( 'Invalid') ) {
-        setMessage( data.message );
-        setAlertType( 'danger' );
-        setCompleted( true );
-      } else
-        setEmail( data.email );
-        if ( data.message === 'update' ) {
-          setOperation( 'Update' );
-        }
-    })
-    .catch( (err: any) => {
-      console.log( "There was a problem fetching the account request.");
-    });
-  }, []);
 
   const handleSubmit = async (e: FormEvent) => {
       e.preventDefault();
       const element = e.currentTarget as HTMLFormElement;
       if ( element.checkValidity() ) {
+        props.setFormState( FORM_STATE_WAITING );
         ReactGA.event({
           category: "Account",
-          action: operation,
+          action: props.operation,
         });
-        setDisableButton( true );
         element.classList.remove('was-validated');
         fetch(
           process.env.REACT_APP_BACKEND_URL + '/accounts',
@@ -66,23 +65,78 @@ function MySQLAccountVerify(props: VerifyProps) {
             },
             body: JSON.stringify( {
               password,
-              token: props.match.params.token
+              token: props.token
             } )
           }
         )
         .then( (response: any) => response.json() )
         .then( (data: any) => {
-          setMessage( data.message );
-          setPassword('');
-          setCompleted( true );
+          props.setMessage( data.message );
+          props.setFormState( FORM_STATE_SUCCESS );
+
         })
         .catch( (err: any) => {
-          console.log( "There was a problem creating a new account request.");
+          console.log( "There was a problem creating a new account.");
+          props.setFormState( FORM_STATE_ERROR );
         });
       } else {
         element.classList.add('was-validated');
       }
   }
+
+  return(
+    <div className='col-md-12 order-md-1'>
+      <form onSubmit={handleSubmit} noValidate>
+        <div className="row">
+          <div className="col mb-3">
+            <label htmlFor="password">Password</label>
+            <input type="password" className="form-control" id="password"
+              value={password} onChange={e => setPassword( e.target.value )}
+              required />
+            <div className="invalid-feedback">
+              A password is required.
+            </div>
+          </div>
+        </div>
+        <button className="btn btn-primary btn-lg btn-block"
+                type="submit">{props.operation} MySQL Account</button>
+      </form>
+    </div>
+
+  );
+}
+
+function MySQLAccountVerify(props: VerifyProps) {
+  const [formState, setFormState] = useState( FORM_STATE_LOADING );
+  const [operation, setOperation] = useState( 'Create' );
+  const [email, setEmail] = useState( '' );
+  const [message, setMessage] = useState( 'OK' );
+
+  useEffect(()=>{
+    fetch( process.env.REACT_APP_BACKEND_URL +
+      '/requests/' +
+      props.match.params.token )
+    .then( (response: any) => response.json() )
+    .then( (data: any) => {
+      console.log( data );
+      if ( data.message.startsWith( 'Invalid') ) {
+        setMessage( data.message );
+        setFormState( FORM_STATE_ERROR );
+      } else {
+        setEmail( data.email );
+        if ( data.message === 'update' ) {
+          setOperation( 'Update' );
+        }
+        setFormState( FORM_STATE_DATA_ENTRY );
+      }
+    })
+    .catch( (err: any) => {
+      console.log( "There was a problem fetching the account request.");
+      setMessage( 'There was a problem. Come back later and try again.');
+      setFormState( FORM_STATE_ERROR );
+    });
+  }, []);
+
 
   return (
     <div>
@@ -92,27 +146,19 @@ function MySQLAccountVerify(props: VerifyProps) {
       </div>
 
       <div className="row">
-        <div className={formDivClass}>
-          <form onSubmit={handleSubmit} noValidate>
-            <div className="row">
-              <div className="col mb-3">
-                <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" id="password"
-                  value={password} onChange={e => setPassword( e.target.value )}
-                  required />
-                <div className="invalid-feedback">
-                  A password is required.
-                </div>
-              </div>
-            </div>
-            <button className="btn btn-primary btn-lg btn-block"
-                    disabled={disableButton}
-                    type="submit">{operation} MySQL Account</button>
-          </form>
-        </div>
-        <div className={alertClass} role="alert">
-          {message}
-        </div>
+        { formState === FORM_STATE_DATA_ENTRY && <MySQLAccountVerifyForm
+                                                    setFormState={setFormState}
+                                                    token={props.match.params.token}
+                                                    setMessage={setMessage}
+                                                    operation={operation} />}
+        { formState === FORM_STATE_WAITING && <MySQLAccountVerifyWaiting
+                                                    message={'processing...'}/>}
+        { formState === FORM_STATE_SUCCESS && <MySQLAccountVerifySuccess
+                                                    message={message}/>}
+        { formState === FORM_STATE_ERROR && <MySQLAccountVerifyError
+                                                    message={message}/>}
+        { formState === FORM_STATE_LOADING && <MySQLAccountVerifyWaiting
+                                                    message={'loading...'}/>}
       </div>
 
     </div>
